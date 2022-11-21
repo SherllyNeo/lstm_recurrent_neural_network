c41665cc509ef9296e04a40d855387c06b92334c
User <jacobp.willo@gmail.com>
2022-11-21 21:07:08 +0000

    fix: made cleaner example

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from lstm_rnn import text_generator
 
 
-path_to_text = "./text.txt"
-start_string = "how dare thee"
-predicatbility = 0.5
-num_generate = 1000
-nlp_ai = text_generator(path_to_text)
-text_gen = nlp_ai.main(start_string,predicatbility,num_generate)
-print(f"\n \n GENERATED TEXT: \n \n {text_gen} \n")
+def gen_test():
+    path_to_text = "./text.txt"
+    start_string = "how dare thee"
+    predicatbility = 0.5
+    num_generate = 1000
+    nlp_ai = text_generator(path_to_text)
+    text_gen = nlp_ai.main(start_string,predicatbility,num_generate)
+    print(f"\n \n GENERATED TEXT: \n \n {text_gen} \n \n")
+gen_test()
